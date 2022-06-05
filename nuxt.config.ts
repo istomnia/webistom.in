299b63ca73475cd493f98be3fbd9037794148f89
webistomin <webistomin@gmail.com>
2022-06-05 13:26:40 +0300

    feat: 🎸 add ssr build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  pages: true,
+  ssr: true,
+  target: 'static',
   content: {
-    navigation: false,
+    // navigation: false,
   },
   modules: ['@nuxt/content']
 })
