85219fa04ceb29153a2b1a17994d4c3078bf6d1e
webistomin <webistomin@gmail.com>
2022-06-05 13:05:00 +0300

    feat: 🎸 init content module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  pages: true,
+  content: {
+    navigation: false,
+  },
+  modules: ['@nuxt/content']
 })
---
+++ pages/test/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    TEST
+
+    <ContentList v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <pre>{{ article }}</pre>
+      </div>
+    </ContentList>
+
+    <content-doc path="work" />
+  </main>
+</template>
